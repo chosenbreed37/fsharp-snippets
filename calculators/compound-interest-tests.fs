cf6b59817b667812e8c6c4af6c5356b87bed4705
Eric Lubisse <chosenbreed@gmail.com>
2015-06-23 14:00:02 +0100

    Added calculator project withi initial calculation of compound interest.

---
+++ calculators/compound-interest-tests.fs
@@ -0,0 +1,10 @@
+﻿module CalculatorTests
+
+open Xunit
+
+[<Fact>]
+let ``10000 compounded at 10% over 10 years yields `` () =
+    let expected = 25937.424601
+    let actual = Calculator.compound 10000.0 0.10 10
+    Assert.Equal(expected, actual)
+
---
+++ calculators/compound-interest.fs
@@ -0,0 +1,6 @@
+﻿module Calculator
+
+let rec compound (principal:float) (interest:float) (term:int) =
+    match term with
+    |0 -> principal
+    |_ -> compound (principal * interest + principal) interest (term - 1)
