5b7165ca49620374ebbd5b0f7a4828cb1c763f35
chosenbreed37 <chosenbreed@gmail.com>
2015-07-06 05:49:42 +0100

    Adding search facility...

---
+++ sr/sr/SearchFiles.fs
@@ -0,0 +1,10 @@
+﻿//module SearchFiles
+//
+//open System.IO
+//
+//let getFiles path pattern =
+//    try
+//        Some Directory.EnumerateFiles(path, pattern, SearchOption.AllDirectories)
+//    with
+//        |DirectoryNotFoundException -> None
+//
