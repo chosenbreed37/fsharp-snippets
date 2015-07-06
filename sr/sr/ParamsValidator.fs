6ab948301cd0ec017a2fe48efb073e1b8680ffe8
chosenbreed37 <chosenbreed@gmail.com>
2015-07-06 05:52:10 +0100

    Added validation tests

---
+++ sr/sr/ParamsValidator.fs
@@ -0,0 +1,36 @@
+﻿module ParamsValidator
+
+open Types
+
+let validateSearchTerm input =
+    if input.Term = "" then
+        Failure InvalidSearchTerm
+    else
+        Success input
+
+let validateReplacementTerm input =
+    if input.Replacement = "" then
+        Failure InvalidReplacementTerm
+    else
+        Success input
+
+let validateFilePath input =
+    if input.Path = "" then
+        Failure InvalidFilePath
+    else
+        Success input
+
+let bind switchFunction twoTrackInput =
+    match twoTrackInput with
+    |Success s -> switchFunction s
+    |Failure f -> Failure f
+
+let (>>=) twoTrackInput switchFunction =
+    bind switchFunction twoTrackInput
+
+let Validate input =
+    input 
+    |> validateSearchTerm 
+    >>= validateReplacementTerm
+    >>= validateFilePath
+    
---
+++ sr/sr/ParamsValidatorTests.fs
@@ -0,0 +1,25 @@
+﻿module ParamsValidatorTests
+
+open Xunit
+open Types
+
+[<Fact>]
+let `` Detects missing search term `` () =
+    let result = @"-r bar -p d:\temp\*.config" |> ParamsParser.Parse |> ParamsValidator.Validate
+    match result with
+    |Success _ -> failwith "Validation failure expected"
+    |Failure f -> Assert.Equal(InvalidSearchTerm, f)
+
+[<Fact>]
+let `` Detects missing replacement term `` () =
+    let result = @"-t foo -p d:\temp\*.config" |> ParamsParser.Parse |> ParamsValidator.Validate
+    match result with
+    |Success _ -> failwith "Validation failure expected"
+    |Failure f -> Assert.Equal(InvalidReplacementTerm, f)
+
+[<Fact>]
+let `` Detects missing path `` () =
+    let result = @"-t foo -r bar" |> ParamsParser.Parse |> ParamsValidator.Validate
+    match result with
+    |Success _ -> failwith "Validation failure expected"
+    |Failure f -> Assert.Equal(InvalidFilePath, f)
